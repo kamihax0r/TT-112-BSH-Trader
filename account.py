cd7e5866291d94fa5b85234ab40e189e08370de0
kamihax0r <anothermatthewmiller@gmail.com>
2024-06-26 14:37:37 -0400

    Account greeks and greeks lookup for single account now working. This code will also show you how to loop positions.

--- account.py
+++ account.py
@@ -1,20 +1,19 @@
 import requests
-from session_manager import SessionManager
 from constants import BASE_URL
-from datetime import datetime
 
 class Account:
-    def __init__(self, account_number):
+    def __init__(self, account_number, session_manager):
         self.account_number = account_number
-        self.session = SessionManager()
+        self.session_manager = session_manager
         self.info = {}
         self.trading_status = {}
         self.positions = []
         self.balance = {}
+        self.greeks = {'delta': 0, 'theta': 0, 'vega': 0, 'gamma': 0}
 
     def get_info(self):
         url = f'{BASE_URL}/customers/me/accounts/{self.account_number}'
-        headers = self.session.get_headers()
+        headers = self.session_manager.get_headers()
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         self.info = response.json().get('data', {})
@@ -22,7 +21,7 @@ class Account:
 
     def get_trading_status(self):
         url = f'{BASE_URL}/accounts/{self.account_number}/trading-status'
-        headers = self.session.get_headers()
+        headers = self.session_manager.get_headers()
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         self.trading_status = response.json().get('data', {})
@@ -30,7 +29,7 @@ class Account:
 
     def get_positions(self):
         url = f'{BASE_URL}/accounts/{self.account_number}/positions'
-        headers = self.session.get_headers()
+        headers = self.session_manager.get_headers()
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         self.positions = response.json().get('data', {}).get('items', [])
@@ -38,41 +37,19 @@ class Account:
 
     def get_balance(self):
         url = f'{BASE_URL}/accounts/{self.account_number}/balances'
-        headers = self.session.get_headers()
+        headers = self.session_manager.get_headers()
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         self.balance = response.json().get('data', {})
         return self.balance
 
-    def get_transactions(self, start_date=None, end_date=None, params=None):
-        url = f'{BASE_URL}/accounts/{self.account_number}/transactions'
-        headers = self.session.get_headers()
-        
-        if params is None:
-            params = {}
-        
-        if start_date:
-            params['start-date'] = start_date
-        if end_date:
-            params['end-date'] = end_date
-        
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json().get('data', {}).get('items', [])
-
-    def get_transaction_by_id(self, transaction_id):
-        url = f'{BASE_URL}/accounts/{self.account_number}/transactions/{transaction_id}'
-        headers = self.session.get_headers()
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json().get('data', {})
-
-    def get_total_fees(self, date=None):
-        url = f'{BASE_URL}/accounts/{self.account_number}/transactions/total-fees'
-        headers = self.session.get_headers()
-        if date is None:
-            date = datetime.now().strftime('%Y-%m-%d')
-        params = {'date': date}
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.json().get('data', {})
+    def get_account_greeks(self):
+        self.get_positions()
+        self.greeks = {'delta': 0, 'theta': 0, 'vega': 0, 'gamma': 0}
+        for pos in self.positions:
+            if 'greeks' in pos:
+                self.greeks['delta'] += pos['greeks'].get('delta', 0)
+                self.greeks['theta'] += pos['greeks'].get('theta', 0)
+                self.greeks['vega'] += pos['greeks'].get('vega', 0)
+                self.greeks['gamma'] += pos['greeks'].get('gamma', 0)
+        return self.greeks
